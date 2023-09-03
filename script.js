771cf5fe79d617c39c7e187793e3fc0a082c84d5
Elippsis007 <michaeldrmm@gmail.com>
2023-09-03 11:49:59 +0100

    practicing variable assignment and basic math

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Hey HTML document, I want to get an element, and I want to get it by and ID is "count-el"
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+// let myAge = 36
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge);
