fe752879b27a6128135b58e49a301e39cbfd6cba
Elippsis007 <michaeldrmm@gmail.com>
2023-09-03 12:46:50 +0100

    created function that when button is clicked it +

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// Initialize count as 0
+// Listen for clicks on increment button
+// Increment the count variable when the button is clicked
+// Change the "count-el" in the html file to reflect the new count
+
 // Hey HTML document, I want to get an element, and I want to get it by and ID is "count-el"
 // document.getElementById("count-el").innerText = 5
 
-// let firstBatch = 5
-// let secondBatch = 7
+let countEl = document.getElementById("count-el");
 
-// let count = firstBatch + secondBatch
+// Assigning 0 to count
+let count = 0;
 
-// console.log(count)
-
-// let myAge = 36
-// let humanDogRatio = 7
-
-// let myDogAge = myAge * humanDogRatio
-
-// console.log(myDogAge);
+// created function called increment linked to "on-click" called "increment"
+function increment() {
+    // count(0) is now.... 0 + 1 = 1 --> each time the button is clicked it will add one
+    count = count + 1;
+    countEl.innerText = count;
+}
